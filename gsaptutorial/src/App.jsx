e678d5223c16cbf0fe3bf6ffac2e3f2f6c41126b
abhaythanak <abhaythanak@gmail.com>
2025-06-21 16:46:55 +0530

    final commit with scrolling video playing

--- gsaptutorial/src/App.jsx
+++ gsaptutorial/src/App.jsx
@@ -1,8 +1,21 @@
-
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Cocktails from "./components/Cocktails";
+import About from "./components/About";
+import Art from "./components/Art";
+import Menu from "./components/Menu";
+import Contact from "./components/Contact";
 
 export default function App() {
   return (
-    <>
-	</>
+    <main>
+      <Navbar />
+      <Hero />
+      <Cocktails />
+      <About />
+      <Art />
+      <Menu />
+      <Contact />
+    </main>
   );
 }
